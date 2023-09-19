bfaf6cb698aaa8dc36f910b48856df108fdad55b
majer565 <majewskifilip2k@gmail.com>
2023-09-19 19:20:05 +0200

    Fix changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,21 +5,6 @@ import type { AppProps } from "next/app";
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <Box
-        component="span"
-        sx={{
-          width: 300,
-          height: 300,
-          fontFamily: theme.typography.fontFamily, // Use theme.typography.fontFamily
-          backgroundColor: theme.palette.secondary.light, // Use theme.palette.secondary.light
-          '&:hover': {
-            backgroundColor: theme.palette.primary.main, // Use theme.palette.primary.main
-            opacity: [0.9, 0.8, 0.7],
-          },  
-        }}
-      >
-        <p>hello world</p>
-      </Box>
       <Component {...pageProps} />
     </ThemeProvider>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,7 @@
-import React from "react";
-import Head from "next/head";
-import Image from "next/image";
-
-import styles from "./index.module.css";
+import theme from "constants/color-palette";
 
 export default function Home() {
   return (
-    <div>
-      hello world
-    </div>
+    <div style={{ fontFamily: theme.typography.fontFamily }}>hello world</div>
   );
 }
