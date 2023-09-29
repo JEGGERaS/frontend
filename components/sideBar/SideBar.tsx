bec9cfde12b5701da52eeab0d1928a7653c85088
majer565 <majewskifilip2k@gmail.com>
2023-09-29 11:42:29 +0200

    Update jest, update tests

--- components/sideBar/SideBar.tsx
+++ components/sideBar/SideBar.tsx
@@ -4,70 +4,11 @@ import GridViewRoundedIcon from "@mui/icons-material/GridViewRounded";
 import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import NotificationsRoundedIcon from "@mui/icons-material/NotificationsRounded";
 import PeopleAltRoundedIcon from "@mui/icons-material/PeopleAltRounded";
-import {
-  Box,
-  ListItemIcon,
-  MenuItem,
-  MenuList,
-  Typography,
-  useTheme,
-} from "@mui/material";
-import { tokens } from "constants/color-palette";
+import { Box, MenuList, Typography, useTheme } from "@mui/material";
 import React from "react";
+import { tokens } from "../../constants/color-palette";
 import HorizontalDivider from "../common/HorizontalDivider";
-
-interface ItemProps {
-  title: string;
-  icon: React.ReactNode;
-  selected: string;
-  setSelected: (title: string) => void;
-}
-
-const Item = (props: ItemProps) => {
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
-
-  return (
-    <MenuItem
-      selected={props.selected === props.title}
-      sx={{
-        color: colors.white[500],
-        marginTop: "1rem",
-        borderLeft: "4px solid transparent",
-        height: "2.5rem",
-        width: "40%",
-        transition: "all 0.3s",
-        "&:hover": {
-          width: "85%",
-          bgcolor: colors.white[500],
-          color: theme.palette.primary.main,
-          borderRadius: "0 2rem 2rem 0",
-          borderLeft: `4px solid ${theme.palette.secondary.main}`,
-        },
-        "&.Mui-selected": {
-          width: "85%",
-          bgcolor: colors.white[500],
-          color: theme.palette.primary.main,
-          borderRadius: "0 2rem 2rem 0",
-          borderLeft: `4px solid ${theme.palette.secondary.main}`,
-        },
-        "&.Mui-selected:hover": {
-          width: "85%",
-          bgcolor: colors.white[500],
-          color: theme.palette.primary.main,
-          borderRadius: "0 2rem 2rem 0",
-          borderLeft: `4px solid ${theme.palette.secondary.main}`,
-        },
-      }}
-      onClick={() => props.setSelected(props.title)}
-    >
-      <ListItemIcon sx={{ color: "inherit" }}>{props.icon}</ListItemIcon>
-      <Typography fontWeight="700" color="inherit">
-        {props.title}
-      </Typography>
-    </MenuItem>
-  );
-};
+import Item from "./Item";
 
 const SideBar = () => {
   const [selected, setSelected] = React.useState<string>("HOME");
