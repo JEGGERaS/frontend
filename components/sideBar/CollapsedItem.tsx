fff22b93618e94d8e5a8088359591994129623cf
majer565 <majewskifilip2k@gmail.com>
2023-09-30 20:02:30 +0200

    Add collapsion

--- components/sideBar/CollapsedItem.tsx
+++ components/sideBar/CollapsedItem.tsx
@@ -1,42 +1,33 @@
-import {
-  alpha,
-  Collapse,
-  ListItemIcon,
-  MenuItem,
-  Typography,
-  useTheme,
-} from "@mui/material";
+import { ListItemIcon, MenuItem, Typography, useTheme } from "@mui/material";
 import { tokens } from "../../constants/color-palette";
 
 interface ItemProps {
-  collapsed: boolean;
-  title: string;
+  id: string;
   icon: React.ReactNode;
   selected: string;
   setSelected: (title: string) => void;
 }
 
-const Item = (props: ItemProps) => {
+const CollapsedItem = (props: ItemProps) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
   return (
     <MenuItem
-      selected={props.selected === props.title}
+      selected={props.selected === props.id}
       sx={{
         color: colors.white[500],
         marginTop: "1rem",
         borderLeft: "4px solid transparent",
         height: "2.5rem",
+        width: "40%",
+        transition: "all 0.3s",
         "&:hover": {
-          animation: "expandWidth 0.5s forwards",
-          //bgcolor: alpha(colors.white[500], 0.9),
+          width: "85%",
           bgcolor: colors.white[500],
           color: theme.palette.primary.main,
           borderRadius: "0 2rem 2rem 0",
           borderLeft: `4px solid ${theme.palette.secondary.main}`,
-          //transition: " background-color 0.5s",
-          transition: "color 0.2s",
         },
         "&.Mui-selected": {
           width: "85%",
@@ -47,31 +38,17 @@ const Item = (props: ItemProps) => {
         },
         "&.Mui-selected:hover": {
           width: "85%",
-          animation: "none",
           bgcolor: colors.white[500],
           color: theme.palette.primary.main,
           borderRadius: "0 2rem 2rem 0",
           borderLeft: `4px solid ${theme.palette.secondary.main}`,
         },
-        "@keyframes expandWidth": {
-          from: {
-            width: "0%",
-          },
-          to: {
-            width: "85%",
-          },
-        },
       }}
-      onClick={() => props.setSelected(props.title)}
+      onClick={() => props.setSelected(props.id)}
     >
       <ListItemIcon sx={{ color: "inherit" }}>{props.icon}</ListItemIcon>
-      <Collapse in={!props.collapsed} orientation="horizontal" timeout={500}>
-        <Typography fontWeight="700" color="inherit">
-          {props.title}
-        </Typography>
-      </Collapse>
     </MenuItem>
   );
 };
 
-export default Item;
+export default CollapsedItem;
--- components/sideBar/SideBar.tsx
+++ components/sideBar/SideBar.tsx
@@ -4,7 +4,14 @@ import GridViewRoundedIcon from "@mui/icons-material/GridViewRounded";
 import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import NotificationsRoundedIcon from "@mui/icons-material/NotificationsRounded";
 import PeopleAltRoundedIcon from "@mui/icons-material/PeopleAltRounded";
-import { Box, MenuList, Typography, useTheme } from "@mui/material";
+import {
+  Box,
+  Button,
+  Collapse,
+  MenuList,
+  Typography,
+  useTheme,
+} from "@mui/material";
 import React from "react";
 import { tokens } from "../../constants/color-palette";
 import HorizontalDivider from "../common/HorizontalDivider";
@@ -12,17 +19,25 @@ import Item from "./Item";
 
 const SideBar = () => {
   const [selected, setSelected] = React.useState<string>("HOME");
+  const [isCollapsed, setIsCollapsed] = React.useState<boolean>(false);
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
+  const handleCollapse = () => {
+    setIsCollapsed((prev) => !prev);
+  };
+
   return (
     <Box
-      width="15rem"
+      width={isCollapsed ? "4rem" : "15rem"}
       height="100vh"
       bgcolor={theme.palette.primary.main}
       display="flex"
       flexDirection="column"
       boxShadow="2"
+      sx={{
+        transition: "width linear 0.3s",
+      }}
     >
       <Box
         width="100%"
@@ -30,50 +45,67 @@ const SideBar = () => {
         display="flex"
         justifyContent="center"
         alignItems="center"
+        overflow="clip"
       >
-        <Typography variant="h1" fontWeight="bold" color={colors.black[900]}>
-          JEGGER
-        </Typography>
+        <Collapse
+          in={!isCollapsed}
+          orientation="horizontal"
+          collapsedSize={20}
+          timeout={700}
+        >
+          <Typography variant="h1" fontWeight="bold" color={colors.black[900]}>
+            JEGGER
+          </Typography>
+        </Collapse>
       </Box>
       <HorizontalDivider bgColor={colors.white[500]} variant="middle" />
       <MenuList sx={{ marginTop: "2rem" }}>
         <Item
+          collapsed={isCollapsed}
           title="HOME"
           icon={<HomeRoundedIcon fontSize="small" />}
           selected={selected}
           setSelected={setSelected}
         />
         <Item
+          collapsed={isCollapsed}
           title="DASHBOARD"
           icon={<GridViewRoundedIcon fontSize="small" />}
           selected={selected}
           setSelected={setSelected}
         />
         <Item
+          collapsed={isCollapsed}
           title="ROZLICZENIA"
           icon={<DescriptionRoundedIcon fontSize="small" />}
           selected={selected}
           setSelected={setSelected}
         />
         <Item
+          collapsed={isCollapsed}
           title="DEALERZY"
           icon={<PeopleAltRoundedIcon fontSize="small" />}
           selected={selected}
           setSelected={setSelected}
         />
         <Item
+          collapsed={isCollapsed}
           title="ZADANIA"
           icon={<AssignmentRoundedIcon fontSize="small" />}
           selected={selected}
           setSelected={setSelected}
         />
         <Item
+          collapsed={isCollapsed}
           title="POWIADOMIENIA"
           icon={<NotificationsRoundedIcon fontSize="small" />}
           selected={selected}
           setSelected={setSelected}
         />
       </MenuList>
+      <Button variant="contained" color="warning" onClick={handleCollapse}>
+        COLLAPSE
+      </Button>
     </Box>
   );
 };
